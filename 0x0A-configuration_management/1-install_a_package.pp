3b4127bd22af019587bcd2818c35b053c1c0f943
Benie2002 <isharabenie@gmail.com>
2023-11-29 05:35:08 -0800

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# installs the package puppet-lint
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
